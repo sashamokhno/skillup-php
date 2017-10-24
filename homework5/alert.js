7e0297223871daf1719501a56e9dc96a4ccac646
Alexander <sasha.mokhno>
2017-10-24 08:27:15 +0300

    homework 5

---
+++ homework5/alert.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var c=15, d=2;
+var result=c+d;
+alert(result);
+
+var a=10,b=2,c=5;
+alert (a+b+c);
+
+var a=17, b=10;
+var c=a-b;
+var d=7;
+var result=c+d;
+alert (result);
+
+var str= "Привет, Мир";
+alert(str);
+
+var str1="Привет, ",str2="Мир";
+alert (str1+str2);
+
+var name=" Sasha";
+alert("Привет,"+name+"!");
+
+var age=29;
+alert("Мне "+age+" лет!");
