e7863ed408a27bc0482fe4c84112868090af27a2
Alexander <sasha.mokhno>
2017-11-03 21:07:09 +0200

    lesson9

---
+++ Lesson9/index.php
@@ -0,0 +1,25 @@
+\*
+<?php
+$arr = ['html', 'css', 'php', 'js', 'jq'];
+    foreach ($arr as $elem)
+    {echo $elem. PHP_EOL;
+    };
+?>
+
+
+
+<?php
+$arr1 = ['10', '20', '15', '17', '24','35'];
+$result=0;
+    foreach ($arr1 as $elem1)
+    {
+        $result += $elem1;
+    }
+    echo $result;
+?>
+
+
+
+
+
+
